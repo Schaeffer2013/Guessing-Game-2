198c19abad53b6007179c604b2441b67ef1aeac5
Schaeffer2013 <135726311+Schaeffer2013@users.noreply.github.com>
2023-06-09 18:51:35 -0400

    Add files via upload

--- guessing_game-2.py
+++ guessing_game-2.py
@@ -3,53 +3,58 @@ import random
 import sys
 from statistics import mean, median, mode, multimode
 
-min_number = 1
-max_number = 100
-random_number = random.randint(min_number, max_number)
-guess_number = []
 
 def start_game():
-    start_game()
-    print("Hello, Player")
 
-    number = int(input("Choose a number between 1 and 100:    "))
-    attempt_count = 1
-    while guess_number != 0:
+    print("Hello, Player, please choose a number between 0 and 100")
+
+    answer = random.randint(0, 100)
+    number_of_guesses = []
+
+    while answer:
             try: 
-                guess_number = int(input())
-                if guess_number < 0 or guess_number > 100:
+                player_guess = int(input("> "))
+                if player_guess < 0 or player_guess > 100:
                     raise ValueError
             except ValueError as err:
                 print("Sorry invalid guess, please choose a number between 1 and 100, tyr again".format(err))
+            else:
+                print("")
+            if player_guess > answer:
+                print("Too high, try again")
+                number_of_guesses.append(player_guess)
                 continue
-                if guess_number > random_number:
-                    print("Too high, try again")
-                    guess_number.append(number)
-                    break
-                elif guess_number < random_number:
-                    print("Too low, try again")
-                    guess_number.append(number)
-                    break
+            elif player_guess < answer:
+                print("Too low, try again")
+                number_of_guesses.append(player_guess)
+                continue
+            else:
+                 print("That is correct, you guessed the right number in {} attempt(s), great job!".format(len(number_of_guesses)))
 
-     
-    print("That is correct, you guessed the right number in {} attempts, great job! Would you like to try again?".format(len(guess_number)))
+                 print("Would you like to play again? Y/N    ")
+                 proceed = input()
+                 while proceed:
+                            if proceed.lower() == 'y':
+                                start_game()
+                            elif proceed.lower() == 'n':
+                                break
+                 print("Thanks for trying the number guessing game, hope you enjoyed it!")
+                 break
+            
+    print("Here are your statistics for the game")
+    min_number = min(number_of_guesses)
+    print(f"The lowest number you guessed was:   {min_number}")
+    max_number = max(number_of_guesses)
+    print(f"The highest number you guessed was:    {max_number}")
+    player_mean = mean(number_of_guesses)
+    print(f"The average of all your guesses is:    {player_guess}")
+    player_median = median(number_of_guesses)
+    print(f"The middle number you guessed was:    {player_median}")
+    player_mode = mode(number_of_guesses)
+    print(f"The most frequent number you guessed was:    {player_mode}")
 
-    while guess_number:
-        if start_game.lower() == "y":
-            continue
-        elif start_game.lower == () == "n":
-            print("Thanks for trying the number guessing game, hope you enjoyed it!")
-            break
-    attempt_count += 1
-    min_number = min(guess_number)
-    print(f"The lowest number guessed was:   {min_number}")
-    max_number = max(guess_number)
-    print(f"The highest number guessed was:    {max_number}")
-    guess_mean = round.mean(guess_number)
-    print(f"The average of all your guesses is:    {guess_mean}")
-    guess_median = round.median(guess_number)
-    print(f"The middle number you guessed was:    {guess_median}")
-    guess_mode = round.mode(guess_number)
-    print(f"The most frequent number you guessed was:    {guess_mode}")
+    
+
+start_game()
 
 
