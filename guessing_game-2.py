a58f2809fc77b1364e8d4e40f427ae0fac1aa8fb
Schaeffer2013 <135726311+Schaeffer2013@users.noreply.github.com>
2023-06-09 11:02:31 -0400

    Add files via upload

--- guessing_game-2.py
+++ guessing_game-2.py
@@ -1,14 +1,6 @@
-"""Psuedo-code Hints
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Save their attempt number to a list.
-    6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list.
-    7. Ask the player if they want to play again.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+import math
 import random
+import sys
 from statistics import mean, median, mode, multimode
 
 min_number = 1
@@ -26,22 +18,38 @@ def start_game():
             try: 
                 guess_number = int(input())
                 if guess_number < 0 or guess_number > 100:
-                    raise ValueError("Sorry invalid guess. Please pick a number between number between 1 and 50.")
-        
-                if guess_number > 8:
+                    raise ValueError
+            except ValueError as err:
+                print("Sorry invalid guess, please choose a number between 1 and 100, tyr again".format(err))
+                continue
+                if guess_number > random_number:
                     print("Too high, try again")
+                    guess_number.append(number)
                     break
-                elif guess_number < 8:
+                elif guess_number < random_number:
                     print("Too low, try again")
+                    guess_number.append(number)
                     break
-        
-    print("That is correct, great job! Would you want to try again?   ")
+
+     
+    print("That is correct, you guessed the right number in {} attempts, great job! Would you like to try again?".format(len(guess_number)))
+
+    while guess_number:
+        if start_game.lower() == "y":
+            continue
+        elif start_game.lower == () == "n":
+            print("Thanks for trying the number guessing game, hope you enjoyed it!")
+            break
     attempt_count += 1
+    min_number = min(guess_number)
+    print(f"The lowest number guessed was:   {min_number}")
+    max_number = max(guess_number)
+    print(f"The highest number guessed was:    {max_number}")
+    guess_mean = round.mean(guess_number)
+    print(f"The average of all your guesses is:    {guess_mean}")
+    guess_median = round.median(guess_number)
+    print(f"The middle number you guessed was:    {guess_median}")
+    guess_mode = round.mode(guess_number)
+    print(f"The most frequent number you guessed was:    {guess_mode}")
 
-answer = input
 
-
-
-
-# Kick off the program by calling the start_game function.
-start_game()
