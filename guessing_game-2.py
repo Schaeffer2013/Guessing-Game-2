c52971094f888abbcb5651bb0ca2588155ca409c
Schaeffer2013 <135726311+Schaeffer2013@users.noreply.github.com>
2023-06-12 15:23:01 -0400

    Add files via upload

--- guessing_game-2.py
+++ guessing_game-2.py
@@ -7,11 +7,14 @@ from statistics import mean, median, mode, multimode
 def start_game():
 
     print("Hello, Player, please choose a number between 0 and 100")
+    print("Current high score is {}".format(len(player_guess)))
 
     answer = random.randint(0, 100)
     number_of_guesses = []
-
-    while answer:
+    attempt_count = 0
+    high_score = []
+    while attempt_count!= 0:
+        while answer:
             try: 
                 player_guess = int(input("> "))
                 if player_guess < 0 or player_guess > 100:
@@ -31,30 +34,36 @@ def start_game():
                 print("Too low, try again")
                 number_of_guesses.append(player_guess)
                 continue
+                attempt_count +=1
             else:
-                 print("That is correct, you guessed the right number in {} attempt(s), great job!".format(len(number_of_guesses)))
+                print(f"That is correct, you guessed the right number in "{}" attempt(s), great job!".format(len(number_of_guesses)))
+                high_score.append(len(player_guess))
+                print("Here are your statistics for the game")
+                min_number = min(number_of_guesses)
+                print(f"The lowest number you guessed was:   {min_number}")
+                max_number = max(number_of_guesses)
+                print(f"The highest number you guessed was:    {max_number}")
+                player_mean = mean(number_of_guesses)
+                print(f"The average of all your guesses is:    {player_guess}")
+                player_median = median(number_of_guesses)
+                print(f"The middle number you guessed was:    {player_median}")
+                player_mode = mode(number_of_guesses)
+                print(f"The most frequent number you guessed was:    {player_mode}")
 
-                 print("Would you like to play again? Y/N    ")
-                 proceed = input()
-                 while proceed:
+                 
+
+
+                print("Would you like to play again? Y/N    ")
+                proceed = input()
+                while proceed:
                             if proceed.lower() == 'y':
                                 start_game()
                             elif proceed.lower() == 'n':
                                 break
-                 print("Thanks for trying the number guessing game, hope you enjoyed it!")
-                 break
-            
-    print("Here are your statistics for the game")
-    min_number = min(number_of_guesses)
-    print(f"The lowest number you guessed was:   {min_number}")
-    max_number = max(number_of_guesses)
-    print(f"The highest number you guessed was:    {max_number}")
-    player_mean = mean(number_of_guesses)
-    print(f"The average of all your guesses is:    {player_guess}")
-    player_median = median(number_of_guesses)
-    print(f"The middle number you guessed was:    {player_median}")
-    player_mode = mode(number_of_guesses)
-    print(f"The most frequent number you guessed was:    {player_mode}")
+                print("Thanks for trying the number guessing game, hope you enjoyed it!")
+                quit
+                 
+    
 
     
 
